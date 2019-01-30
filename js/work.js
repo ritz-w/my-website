6dd8d087aa197e9011418193799d9eae46ac290d
ritz-w <ritz.wu@outlook.com>
2019-01-30 13:25:00 +0800

    added self brand outlet link

--- js/work.js
+++ js/work.js
@@ -30,7 +30,7 @@ var websites = [
   {
     title: "Self Brand Outlet (2018)",
     pic: "img/self-brand-outlet.png",
-    url: "https://locate-her.herokuapp.com/",
+    url: "https://self-brand-outlet.herokuapp.com/",
     blurb: "Conceptual online shop of contemporary artworks turned into merchandise, or merchandise presented by artists as artworks. Explores the different kinds of statements artists make on their relationship to trade, whether they be exercises in satire, democratising their work, fashion, promotion, philanthropy, or something else.",
     tags: ['Node.JS', 'Mongo DB', 'Express', 'React', 'Javascript']
   }
